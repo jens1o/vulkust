92469f9f369ea72af39d71949a76b8c93fdfe14e
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2019-01-30 13:21:48 +0330

    fbp:: --for backup, a little work in graph based rendering--

--- vulkust/src/render/graph/node/mod.rs
+++ vulkust/src/render/graph/node/mod.rs
@@ -3,3 +3,10 @@
 /// Its each node has input and output links.
 /// There are some predifined ids for links, in addition users of engine can specify their ids
 /// Node must bring a structure for its output link, and must accept a trait as its dependancy
+/// 
+
+pub mod shadow_mapper;
+pub mod shadow_accumulator;
+pub mod g_buffer_filler;
+pub mod ssao;
+pub mod ssr;
--- vulkust/src/render/graph/tree/mod.rs
+++ vulkust/src/render/graph/tree/mod.rs
@@ -2,8 +2,15 @@
 /// A `Tree` itself can be a `Node`
 /// A `Tree` can be feeded with several things like Model, Scene, Material, Mesh or other production of other tree
 /// Tree will gather all the needed frame-data, render-data of it nodes
-/// 
-/// 
-/// 
-/// 
-/// 
+
+#[repr(u64)]
+#[cfg_attr(debug_mode, derive(Debug))]
+pub enum TreeType {
+    DeferredPBR = 1,
+    ForwardPBR = 2,
+    Shadower = 3,
+    Unlit = 4,
+}
+
+/// User must start their ids from here
+pub const USER_STARTING_TYPE_ID: Id = 1024 * 1024;
